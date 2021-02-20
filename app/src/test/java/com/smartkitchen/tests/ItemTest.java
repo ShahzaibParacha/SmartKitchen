8ea82d7495afc6bd1c2541e77117519f0f7bd27d
Raven Carencia <carencrm@myumanitoba.ca>
2021-02-20 06:02:48 -0600

    updated ItemTest to reflect changes to Item.

--- app/src/test/java/com/smartkitchen/tests/ItemTest.java
+++ app/src/test/java/com/smartkitchen/tests/ItemTest.java
@@ -11,7 +11,7 @@ public class ItemTest{
         System.out.println("\nStarting testItemConstructor.");
 
         //Act
-        Item testItem = new Item("sampleItem", 1, "sampleUnit", 1);
+        Item testItem = new Item("sampleItem", 1, "sampleUnit", 1, 1);
 
         //Assert
         assertNotNull(testItem);
@@ -19,7 +19,7 @@ public class ItemTest{
         assertEquals(testItem.getQuantity(), 1);
         assertEquals(testItem.getUnits(), "sampleUnit");
         assertEquals(testItem.getQuantityToBuy(), 1);
-        assertEquals(testItem.getInitQuantity(), 1);
+        assertEquals(testItem.getInitQuantity(), 0);
         
         System.out.println("Finished testItemConstructor.");
     }
@@ -29,11 +29,12 @@ public class ItemTest{
         System.out.println("\nStarting testItemSetters.");
 
         //Act
-        Item testItem = new Item("sample", 1, "sampleUnit", 1);
+        Item testItem = new Item("sample", 1, "sampleUnit", 1, 1);
         testItem.setName("sample2");
         testItem.setQuantity(2);
         testItem.setUnits("sampleUnit2");
         testItem.setQuantityToBuy(2);
+        testItem.setThresholdQuantityQuantity(2);
 
         //Assert
         assertEquals(testItem.getName(), "sample2");
@@ -41,6 +42,7 @@ public class ItemTest{
         assertEquals(testItem.getUnits(), "sampleUnit2");
         assertEquals(testItem.getQuantityString(), "2 sampleUnit2");
         assertEquals(testItem.getQuantityToBuyString(), "2 sampleUnit2");
+        assertEquals(testItem.getThresholdQuantity(), 2);
         
         System.out.println("Finished testItemSetters.");
     }
@@ -50,7 +52,7 @@ public class ItemTest{
         System.out.println("\nStarting testItemQuantity.");
 
         //Act
-        Item testItem = new Item("sample", 1, "sampleUnit", 1);
+        Item testItem = new Item("sample", 1, "sampleUnit", 1, 1);
         testItem.setInitQuantity(2);
 
         //Assert
@@ -64,19 +66,35 @@ public class ItemTest{
         System.out.println("\nStarting testThresholdStatus.");
 
         //Act
-        Item testItem = new Item("sample", 1, "sampleUnit", 1);
-        testItem.updateQuantity(0);
+        Item testItem = new Item("sample", 1, "sampleUnit", 1, 1);
+        testItem.setQuantity(0);
 
         //Assert
         assert(testItem.thresholdStatus());
 
         //Act 2 -- update the quantity so it triggers false in the thresholdStatus method
-        testItem.updateQuantity(50);
+        testItem.setQuantity(50);
 
         //Assert
         assertFalse(testItem.thresholdStatus());
 
         System.out.println("Finished testThresholdStatus.");
+    }
+
+    @Test
+    public void testCalculateDefaultThreshold(){
+        System.out.println("\nStarting testCalculateDefaultThreshold.");
+
+        //Act
+        Item testItem = new Item("sample", 10, "sampleUnit", 1, 1);
+
+        //Assert -- we expect 2 because of the 20% threshold const;
+        assertEquals(testItem.calculateDefaultThreshold(), 2);
+
+
+
+        System.out.println("Finished testCalculateDefaultThreshold.");
+
     }
 }
 
