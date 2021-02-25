8268809265dd732d0d4467978d82c86b5e3b5885
Raven Carencia <carencrm@myumanitoba.ca>
2021-02-25 13:38:18 -0600

    Added tests for ListValidation class.

--- app/src/test/java/com/smartkitchen/tests/ListValidationTest.java
+++ app/src/test/java/com/smartkitchen/tests/ListValidationTest.java
@@ -59,5 +59,58 @@ public class ListValidationTest{
         //Assert
         System.out.println("Finished testThresholdStatus.");
     }
+
+    @Test
+    public void testContainsItemInput() throws Exception {
+        System.out.println("\nStarting testContainsItemInput.");
+
+        //Act 1 -- getName
+        Item testItem = new Item("", -1, "", -1, -1);
+        ListValidation testListValidation = new ListValidation(testItem);
+        try {
+            testListValidation.containsItemInputs();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Act 2 -- getQuantity
+        Item testItem1 = new Item(" ", -1, "", -1, -1);
+        ListValidation testListValidation1 = new ListValidation(testItem1);
+        try {
+            testListValidation1.containsItemInputs();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Act 3 -- getLength
+        Item testItem2 = new Item(" ", 1, "", -1, -1);
+        ListValidation testListValidation2 = new ListValidation(testItem2);
+        try {
+            testListValidation2.containsItemInputs();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Act 4 -- threshold
+        Item testItem3 = new Item(" ", 1, " ", 0, -1);
+        ListValidation testListValidation3 = new ListValidation(testItem3);
+        try {
+            testListValidation3.containsItemInputs();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Act 4 -- full true
+        Item testItem4 = new Item("sampleItem", 1, "sampleItem", 1, 1);
+        ListValidation testListValidation4 = new ListValidation(testItem4);
+        try {
+            testListValidation3.containsItemInputs();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Finished testContainsItemInput.");
+    }
+
 }
 
